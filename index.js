9bf164eb64593e6a44ddfeb358744d772f7f96d6
luizwp <148246609+luizwp@users.noreply.github.com>
2024-06-24 22:23:59 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var audio = document.getElementById('audio-player');
+    var playButton = document.getElementById('play');
+    var pauseButton = document.getElementById('pause');
+    var stopButton = document.getElementById('stop');
+
+    playButton.addEventListener('click', function() {
+        audio.play();
+    });
+
+    pauseButton.addEventListener('click', function() {
+        audio.pause();
+    });
+
+    stopButton.addEventListener('click', function() {
+        audio.pause();
+        audio.currentTime = 0;
+    });
+});
